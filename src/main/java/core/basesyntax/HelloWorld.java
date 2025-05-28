a4508787b418070fcdc1256e5feeac1fa2b079a0
Zahar <gmirianskiy@gmail.com>
2025-05-28 23:18:16 +0300

    fix12

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,7 +1,7 @@
-package core.basesyntax.model;
+package core.basesyntax;
 
-import core.basesyntax.model.Cat;
-import core.basesyntax.model.Dog;
+import core.basesyntax.Model.Cat;
+import core.basesyntax.Model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {

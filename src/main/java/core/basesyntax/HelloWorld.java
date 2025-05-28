aa3d3745d5853524a27cb791beb39511ff94712e
Zahar <gmirianskiy@gmail.com>
2025-05-28 22:37:54 +0300

    fix8

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -23,10 +23,12 @@ public class HelloWorld {
             if (i % 2 == 0) {
              result = result + i + " ";
         }
+        }
         return result;
     }
-        private void sayHello(String str)    {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file.  a"
+
+    private void sayHello(String str) {
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file."
         + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(str);
     }

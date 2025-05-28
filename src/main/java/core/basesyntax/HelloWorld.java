22fede9a72f6d86a246ac7864a5886ee8e9482b4
Zahar <gmirianskiy@gmail.com>
2025-05-28 21:41:57 +0300

    fix5

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -26,10 +26,10 @@ public class HelloWorld {
         return result;
     }
 
-    private void sayHello(String HELLO) {
+    private void sayHello(String str) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file."
         + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+        System.out.println(str);
     }
 
     private void initializeVariables() {

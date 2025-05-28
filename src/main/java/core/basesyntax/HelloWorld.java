4ea1404b2f7d819b71e3ab4c4654bcca2468d420
Zahar <gmirianskiy@gmail.com>
2025-05-28 22:22:35 +0300

    fix7

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -27,7 +27,7 @@ import java.time.LocalDate;
          }
 
          private void sayHello(String str){
-             System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file.  a"
+             System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file."
                      + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
              System.out.println(str);
          }

6a01acefd27c65c6dec59988524c7d8ce4ffb09e
Zahar <gmirianskiy@gmail.com>
2025-05-28 22:29:30 +0300

    fix8

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,45 +4,41 @@ import core.basesyntax.Model.Cat;
 import core.basesyntax.Model.Dog;
 import java.time.LocalDate;
 
- public class HelloWorld {
+public class HelloWorld {
 
-     private String str;
-     private int variable;
-     private int age;
-     private int size;
+    private String str;
+    private int variable;
+    private int age;
+    private int size;
 
-     public static void main(String[] args) {
-         Dog dog = new Dog();
-         Cat cat = new Cat();
-     }
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+    }
 
-     private String getFromOneToTen() {
-         String result = " ";
+    private String getFromOneToTen() {
+        String result = " ";
 
-         for (int i = 0; i < 10; i++) {
-             if (i % 2 == 0) {
-                 result = result + i + " ";
-             }
-             return result;
-         }
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+             result = result + i + " ";
+        }
+        return result;
+    }
+        private void sayHello(String str)    {
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file.  a"
+        + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println(str);
+    }
 
-         private void sayHello(String str){
-             System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file."
-                     + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-             System.out.println(str);
-         }
-
-         private void initializeVariables() {
-             String str = "Hello mates!";
-             System.out.println(str);
-             int variable = LocalDate.now().getYear();
-             System.out.println("It is " + variable + " year");
-             int age = 25;
-             System.out.println("I'm " + age);
-             int size = 42;
-             System.out.println("The size is " + size);
-         }
-
-     }
-
- }
+    private void initializeVariables() {
+        String str = "Hello mates!";
+        System.out.println(str);
+        int variable = LocalDate.now().getYear();
+        System.out.println("It is " + variable + " year");
+        int age = 25;
+        System.out.println("I'm " + age);
+        int size = 42;
+        System.out.println("The size is " + size);
+    }
+}
